f40955267a284f4e3affa8cae08ffc13c79969fe
Nat <minimnat@hotmail.com>
2014-02-23 14:36:26 +0000

    Update Application.java

--- Android/src/com/parse/tutorials/pushnotifications/Application.java
+++ Android/src/com/parse/tutorials/pushnotifications/Application.java
@@ -2,6 +2,7 @@ package com.parse.tutorials.pushnotifications;
 
 import com.parse.Parse;
 import com.parse.ParseInstallation;
+import com.parse.ParseAnalytics;
 import com.parse.PushService;
 
 public class Application extends android.app.Application {
@@ -14,8 +15,12 @@ public class Application extends android.app.Application {
     super.onCreate();
 
 	// Initialize the Parse SDK.
-	Parse.initialize(this, "YOUR_APP_ID", "YOUR_CLIENT_KEY"); 
-
+	Parse.initialize(this, "7nAuWaGiKovuuXQmGqUF0foP3RAFhSJ2KpLufbaT", "iXokVvjZf340Ke1pPzShPTLPFsihMUdXP0EDSvZx");
+	
+        PushService.setDefaultPushCallback(this, YourDefaultActivity.class);
+        
+        ParseAnalytics.trackAppOpened(getIntent());
+        
 	// Specify an Activity to handle all pushes by default.
 	PushService.setDefaultPushCallback(this, MainActivity.class);
   }
